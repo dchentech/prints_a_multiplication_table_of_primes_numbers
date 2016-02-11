254f769e2249e6dee3bada25dfbba296be27c945
David Chen <mvjome@gmail.com>
2016-02-11 14:06:43 +0000

    Complete PrimeTable

--- prints_a_multiplication_table_of_primes_numbers/__init__.py
+++ prints_a_multiplication_table_of_primes_numbers/__init__.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["PrimeGenerator", "Table"]
+__all__ = ["PrimeGenerator", "NumLines"]
 
 from .prime_generator import PrimeGenerator
-from .table import Table
+from .num_lines import NumLines
+from .tabulate_ext import tabulate
 
 
 class PrimeTable(object):
-    pass
+    """
+    Generate a multiplication table of primes numbers.
+    """
+
+    @staticmethod
+    def output(number):
+        primes_numbers = PrimeGenerator.generate_n_primes(number)
+        lines = NumLines.generate(primes_numbers)
+        output = tabulate(lines, tablefmt="prime", headers="firstrow")
+        return output
